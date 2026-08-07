f797b2e5d5f1bca39beac0f95d22c9cd6a49f468
Lucas Paguetti Pereira <lucaspaguetti2@gmail.com>
2026-08-07 20:00:43 -0300

    feat: primeiros arquivos em .java. Hello World e Variáveis

---
+++ java/mensagem_java.java
@@ -0,0 +1,8 @@
+public class mensagem_java 
+{
+    public static void main(String[] args) 
+    {
+        String mensagem = "\n\t Primeiro print utilizando o Java ☕️\n";
+        System.out.println(mensagem);
+    }
+}
---
+++ java/ola_mundo.java
@@ -0,0 +1,7 @@
+public class ola_mundo 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Olá, Mundo!");
+    }
+}
