0cbd3371868013196287372f918780371ee8d0a3
Lucas Paguetti Pereira <lucaspaguetti2@gmail.com>
2026-08-08 08:53:33 -0300

    feat: Curso em Video - Java 03/31

---
+++ Curso em Video/java/#01 Curso de Java - História do Java - Gustavo Guanabara.java
@@ -0,0 +1,51 @@
+/*
+ * A História da Linguagem Java se inicia em 1990, quando a Sun Microsystems
+ * criou uma equipe chamada Green Team, liderada por James Gosling.
+ *
+ * Até o momento, as Linguagens de Programação como Pascal e C, precisavam de
+ * compiladores específicos para cada plataforma. Sem isso, os programas só
+ * seriam executáveis para um único sistema.
+ *
+ * Foi aí que o grupo criou o projeto GreenTalk, que virou a Linguagem Oak,
+ * capaz de ligar dispositivos diferentes e fazer com que eles pudessem se
+ * comunicar.
+ *
+ * O Star Seven (*7) surgiu em 1991 e era um aparelho multimídia capaz de
+ * controlar vários dispositivos da sua casa. Infelizmente o projeto não foi
+ * aceito na época e tudo foi engavetado em 1992.
+ *
+ * Em 1994, com o advento da World Wide Web (www) e da Linguagem HTML, o
+ * projeto da Sun foi reestabelecido com o objetivo de criar um Navegador Web
+ * capaz de rodar aplicativos em Oak.
+ *
+ * O nome "Oak" não poderia ser utilizado por questões de direitos
+ * relacionados. A linguagem foi rebatizada em homenagem a uma gíria relativa
+ * a "Café Quente": Java Coffee.
+ *
+ * Surgem então o navegador HotJava e a linguagem Java, que ficou famosa por
+ * conta de matérias veiculadas pela imprensa da época.
+ *
+ * A partir daí, vários projetos surgiram para usar Java em todo lugar. O
+ * Java Ring e Sondas Espaciais da NASA são exemplos disso.
+ *
+ * Em 2006 a Linguagem Java se tornou Open Source, que tem seu código livre
+ * para consultas, pesquisas e personalizações. Java usa a licença GPL
+ * (General Public Licence).
+ *
+ * Em 2009 a Sun Microsystems foi vendida para a Oracle por US$7,4 bilhões.
+ *
+ * Atualmente, a Linguagem Java pode ser encontrada em chips de cartão de
+ * crédito, discos de blu-ray, vídeo games (PS4 roda Java), leitores de
+ * e-books (Kindle roda Java), smartphones (Android roda Java), TV digital
+ * (o padrão Ginga é feito em Java), relógios inteligentes (Moto 360 roda
+ * Java) e até mesmo no programa para a Declaração de Imposto de Renda.
+ */
+
+class HistoriaDoJava
+{
+    public static void main(String[] args) 
+    {
+        String historia = "Leia a Documentação";
+        System.out.println(historia);
+    }
+}
---
+++ Curso em Video/java/#02 Exercícios de Java - Gustavo Guanabara.java
@@ -0,0 +1,7 @@
+class OlaMundo 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Olá, Mundo! em Java ☕️");
+    }
+}
